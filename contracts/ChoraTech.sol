6f0a6a62d3e13a82e5fd7558c051f7fe37999483
Ryan Christoffersen <ryan@ryanchristo.com>
2018-02-25 00:50:12 +0700

    added methods and event to contract

--- contracts/ChoraTech.sol
+++ contracts/ChoraTech.sol
@@ -4,8 +4,30 @@ contract ChoraTech {
 
   address private owner;
 
+  struct Account {
+    string firstName;
+    string lastName;
+  }
+
+  mapping (address => Account) accounts;
+
+  event AccountNameSet(bool success);
+
   function ChoraTech() public {
     owner = msg.sender;
   }
-  
+
+  function getAccount() public constant returns (string, string) {
+    return (
+      accounts[msg.sender].firstName,
+      accounts[msg.sender].lastName
+    );
+  }
+
+  function setAccountName(string firstName, string lastName) public returns (bool) {
+    accounts[msg.sender].firstName = firstName;
+    accounts[msg.sender].lastName = lastName;
+    return true;
+  }
+
 }

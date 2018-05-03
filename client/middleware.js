fa8a77cc7a11b9e575c70494165defea9eb96954
Ryan Christoffersen <ryan@ryanchristo.com>
2018-05-03 16:16:28 +0700

    set redux logger default to collapsed

--- client/middleware.js
+++ client/middleware.js
@@ -2,10 +2,14 @@ import { applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
 
+const logger = createLogger({
+  collapsed: true,
+})
+
 let middleware = [thunk]
 
 if (process.env.NODE_ENV !== 'production') {
-  middleware = [...middleware, createLogger()]
+  middleware = [...middleware, logger]
 }
 
 export default applyMiddleware(...middleware)
